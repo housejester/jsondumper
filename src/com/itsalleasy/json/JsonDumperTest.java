6ef87bfd300b96e055a359002d7d3b2bf71f9c9f
James Estes <james.estes@gmail.com>
2008-08-20 20:52:49 -0600

    Added support for unicode.  Now will output all ascii printable characters and the unicode escape sequence otherwise (spec says any unicode character which should allow any char from java, but we'll do this for now).
    Also removed dependency on commonslang for their stringescapeutils.  it
    wasn't escaping per JSON spec anyhow.

--- src/com/itsalleasy/json/JsonDumperTest.java
+++ src/com/itsalleasy/json/JsonDumperTest.java
@@ -13,134 +13,116 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 public class JsonDumperTest extends TestCase{
+	private JsonDumper dumper;
+	public void setUp(){
+		dumper = new JsonDumper();
+	}
 	public void testShouldDumpStringsQuoted(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = "some string";
 		String dump = dumper.dump(obj);
 		assertEquals("\"some string\"", dump);
 	}
 	public void testShouldDumpIntegers(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = 100;
 		String dump = dumper.dump(obj);
 		assertEquals("100", dump);
 	}
 	public void testShouldTruncateFloatsWithNoDecimal(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = 100f;
 		String dump = dumper.dump(obj);
 		assertEquals("100", dump);
 	}
 	public void testShouldFormatFloatsToTwoDecimalsByDefault(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = 100.33333333f;
 		String dump = dumper.dump(obj);
 		assertEquals("100.33", dump);
 	}
 	public void testShouldRoundUpFloats(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = 100.338f;
 		String dump = dumper.dump(obj);
 		assertEquals("100.34", dump);
 	}
 	public void testShouldRoundUpNonDecimalPartOfFloats(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = 100.999f;
 		String dump = dumper.dump(obj);
 		assertEquals("101", dump);
 	}
 	public void testShouldDumpBooleanTrue(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = true;
 		String dump = dumper.dump(obj);
 		assertEquals("true", dump);
 	}
 	public void testShouldDumpBooleanFalse(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = false;
 		String dump = dumper.dump(obj);
 		assertEquals("false", dump);
 	}
 	public void testShouldDumpNull(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = null;
 		String dump = dumper.dump(obj);
 		assertEquals("null", dump);
 	}
 	public void testShouldDumpArrayOfStrings(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = new String[]{"one", "two"};
 		String dump = dumper.dump(obj);
 		assertEquals("[\"one\",\"two\"]", dump);
 	}
 	public void testShouldDumpEmptyArray(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = new String[]{};
 		String dump = dumper.dump(obj);
 		assertEquals("[]", dump);
 	}
 	public void testShouldDumpArrayOfIntegers(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = new Integer[]{1,2,3,4};
 		String dump = dumper.dump(obj);
 		assertEquals("[1,2,3,4]", dump);
 	}
 	public void testShouldDumpArrayOfInts(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new int[]{1,2,3,4};
 		String dump = dumper.dump(obj);
 		assertEquals("[1,2,3,4]", dump);
 	}
 	public void testShouldDumpArrayOfFloats(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new Float[]{1f,2.1f,3.1f,4.1f};
 		String dump = dumper.dump(obj);
 		assertEquals("[1,2.1,3.1,4.1]", dump);
 	}
 	public void testShouldDumpArrayOfPrimitiveFloats(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new float[]{1f,2.1f,3.1f,4.1f};
 		String dump = dumper.dump(obj);
 		assertEquals("[1,2.1,3.1,4.1]", dump);
 	}
 	public void testShouldDumpArrayOfBooleans(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new Boolean[]{true,true,false,true};
 		String dump = dumper.dump(obj);
 		assertEquals("[true,true,false,true]", dump);
 	}
 	public void testShouldDumpArrayOfPrimitiveBooleans(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new boolean[]{true,true,false,true};
 		String dump = dumper.dump(obj);
 		assertEquals("[true,true,false,true]", dump);
 	}
 	public void testShouldDumpArrayOfNulls(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new Object[]{null,null,null};
 		String dump = dumper.dump(obj);
 		assertEquals("[null,null,null]", dump);
 	}
 	public void testShouldDumpMixedArrayOfNulls(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new Integer[]{1,null,3};
 		String dump = dumper.dump(obj);
 		assertEquals("[1,null,3]", dump);
 	}
 	public void testShouldDumpListCollections(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = Arrays.asList(new Integer[]{1,2,3});
 		String dump = dumper.dump(obj);
 		assertEquals("[1,2,3]", dump);
 	}
 	public void testShouldDumpListOfStrings(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = Arrays.asList(new String[]{"1","2","3"});
 		String dump = dumper.dump(obj);
 		assertEquals("[\"1\",\"2\",\"3\"]", dump);
 	}
 	public void testShouldDumpSetCollections(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new HashSet<Object>(Arrays.asList(new Integer[]{1,2,3}));
 		String dump = dumper.dump(obj);
 		String numbersWithCommas = dump.substring(1, dump.length()-1);
@@ -155,8 +137,8 @@ public class JsonDumperTest extends TestCase{
 		map.put("key1", "value1");
 		map.put("key2", 2);
 		map.put("key3", true);
-		JsonDumper dumper = new JsonDumper();
- 		Object obj = map;
+
+		Object obj = map;
 		String dump = dumper.dump(obj);
 		assertEquals('{', dump.charAt(0));
 		assertEquals('}', dump.charAt(dump.length()-1));
@@ -171,7 +153,6 @@ public class JsonDumperTest extends TestCase{
 		TestBean bean = new TestBean();
 		bean.setName("foo");
 		bean.setAge(22);
-		JsonDumper dumper = new JsonDumper();
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals('{', dump.charAt(0));
@@ -189,14 +170,12 @@ public class JsonDumperTest extends TestCase{
 		parent.setChildren(Collections.singletonList(child));
 		child.setChildren(Collections.singletonList(parent));
 		Object obj = parent;
-		JsonDumper dumper = new JsonDumper();
 		String dump = dumper.dump(obj);
 		assertEquals("{\"children\":[{\"children\":[{\"$ref\":\"\"}]}]}", dump);
 	}
 	public void testShouldBeAbleToDumpMultipleReferencesToSameObjectInCollections(){
 		ParentBean parent = new ParentBean("bean");
 		Object obj = new Object[]{parent, parent};
-		JsonDumper dumper = new JsonDumper();
 		String dump = dumper.dump(obj);
 		assertEquals("[{\"name\":\"bean\"},{\"$ref\":\"0\"}]", dump);
 	}
@@ -216,7 +195,6 @@ public class JsonDumperTest extends TestCase{
 			parents.add(parent);
 		}
 		
-		JsonDumper dumper = new JsonDumper();
  		Object obj = parents;
 		long start = System.currentTimeMillis();
 		String dump = dumper.dump(obj);
@@ -236,7 +214,6 @@ public class JsonDumperTest extends TestCase{
 		cal.set(year, month, day, hour, minute, second);
 		Date date = cal.getTime();
 		long millis = date.getTime();
-		JsonDumper dumper = new JsonDumper();
  		Object obj = date;
 		String dump = dumper.dump(obj);
 		assertEquals("new Date("+millis+")", dump);
@@ -252,36 +229,41 @@ public class JsonDumperTest extends TestCase{
 		cal.set(year, month, day, hour, minute, second);
 		Date date = cal.getTime();
 		long millis = date.getTime();
-		JsonDumper dumper = new JsonDumper();
  		Object obj = cal;
 		String dump = dumper.dump(obj);
 		assertEquals("new Date("+millis+")", dump);
 	}
 	public void testShouldEscapeDoubleQuotesInStringValues(){
-		JsonDumper dumper = new JsonDumper();
 		Object obj = "some \"quoted string\"";
 		String dump = dumper.dump(obj);
 		assertEquals("\"some \\\"quoted string\\\"\"", dump);
+	}
+	public void testShouldEscapeForwardSlashesInStrings(){
+		Object obj = "some string/with/slashes";
+		String dump = dumper.dump(obj);
+		assertEquals("\"some string\\/with\\/slashes\"", dump);
+	}
+	public void testShouldProduceUnicodeForNonUtf8Charset(){
+		Object obj = "\u6C34";
+		String dump = dumper.dump(obj);
+		assertEquals("\"\\u6c34\"", dump);
 	}
 	public void testShouldNotDumpNullBeanProperties(){
 		TestBean bean = new TestBean();
 		bean.setName(null);
 		bean.setAge(22);
-		JsonDumper dumper = new JsonDumper();
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"age\":22}", dump);
 	}
 	public void testShouldNotDumpBooleanFalseBeanProperties(){
 		TestBooleanBean bean = new TestBooleanBean("test", false);
-		JsonDumper dumper = new JsonDumper();
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"name\":\"test\"}", dump);
 	}
 	public void testShouldNotDumpEmptyStringBeanProperties(){
 		TestBooleanBean bean = new TestBooleanBean("", true);
-		JsonDumper dumper = new JsonDumper();
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"value\":true}", dump);
@@ -290,7 +272,6 @@ public class JsonDumperTest extends TestCase{
 		TestBean bean = new TestBean();
 		bean.setName("foo");
 		bean.setAge(0);
-		JsonDumper dumper = new JsonDumper();
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"name\":\"foo\"}", dump);
@@ -299,7 +280,7 @@ public class JsonDumperTest extends TestCase{
 		ParentBean bean = new ParentBean();
 		bean.setName("parent");
 		bean.setChildren(new ArrayList<TestBean>());
-		JsonDumper dumper = new JsonDumper();
+
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"name\":\"parent\"}", dump);
@@ -307,7 +288,7 @@ public class JsonDumperTest extends TestCase{
 	@SuppressWarnings("unchecked")
 	public void testShouldNotDumpEmptyMapBeanProperties(){
 		TestBeanWithMapProperty bean = new TestBeanWithMapProperty("foo", new HashMap());
-		JsonDumper dumper = new JsonDumper();
+
  		Object obj = bean;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"name\":\"foo\"}", dump);
@@ -316,31 +297,27 @@ public class JsonDumperTest extends TestCase{
 		Map<String, Object> map = new HashMap<String,Object>();
 		map.put("age", 22);
 		map.put("name", null);
-		JsonDumper dumper = new JsonDumper();
+
  		Object obj = map;
 		String dump = dumper.dump(obj);
 		assertEquals("{\"age\":22}", dump);
 	}
 	public void testShouldNotHaveCommasInNumbers(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = 100000;
 		String dump = dumper.dump(obj);
 		assertEquals("100000", dump);
 	}
 	public void testShouldDumpBooleanBeanProperties(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = new TestBeanWithBooleanProperty();
 		String dump = dumper.dump(obj);
 		assertEquals("{\"active\":true}", dump);
 	}
 	public void testShouldBeAbleToDumpCharacters(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = 'c';
 		String dump = dumper.dump(obj);
 		assertEquals("\"c\"", dump);
 	}
 	public void testShouldEscapeNewLineCharactersInStrings(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = "line one\nline 2";
 		String dump = dumper.dump(obj);
 		assertEquals("\"line one\\nline 2\"", dump);
@@ -349,11 +326,9 @@ public class JsonDumperTest extends TestCase{
 		SIMPLE_VALUE
 	}
 	public void testShouldDumpEnumsAsStrings(){
-		JsonDumper dumper = new JsonDumper();
  		Object obj = SimpleEnum.SIMPLE_VALUE;
 		String dump = dumper.dump(obj);
 		assertEquals("\"SIMPLE_VALUE\"", dump);
-		
 	}
 	/*
 	 * public void testShouldNotAllowDangerousJavascriptInStrings()
--- src/com/itsalleasy/json/serializers/StringSerializer.java
+++ src/com/itsalleasy/json/serializers/StringSerializer.java
@@ -5,15 +5,54 @@ package com.itsalleasy.json.serializers;
 
 import java.io.IOException;
 
-import org.apache.commons.lang.StringEscapeUtils;
-
 import com.itsalleasy.json.JsonWriter;
 import com.itsalleasy.json.JsonSerializer;
 
 public class StringSerializer implements JsonSerializer {
 	public void toJson(Object obj, JsonWriter context) throws IOException {
 		context.append('"');
-		StringEscapeUtils.escapeJavaScript(context.getWriter(), obj.toString());
+		this.escape(obj.toString(), context);
 		context.append('"');
 	}
+    private void escape(String string, JsonWriter writer) throws IOException {
+        int          len = string.length();
+        for (int i = 0; i < len; i ++) {
+            char c = string.charAt(i);
+            switch (c) {
+            case '"':
+            case '\\':
+            case '/':
+                writer.append('\\');
+                writer.append(c);
+                break;
+            case '\b':
+                writer.append("\\b");
+                break;
+            case '\f':
+                writer.append("\\f");
+                break;
+            case '\n':
+                writer.append("\\n");
+                break;
+            case '\r':
+                writer.append("\\r");
+                break;
+            case '\t':
+                writer.append("\\t");
+                break;
+            default:
+                if (c < '\u0020' || c >= '\u007F'){
+                	writer.append("\\u");
+                	String t = Integer.toHexString(c);
+                	int numZeroesNeeded = 4 - t.length();
+                	for(int zeroNum=0; zeroNum<numZeroesNeeded; zeroNum++){
+                		writer.append('0');
+                	}
+                    writer.append(t);
+                } else {
+                    writer.append(c);
+                }
+            }
+        }
+    }
 }
